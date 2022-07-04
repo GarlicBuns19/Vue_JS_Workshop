e1031d85fb5a386bd39639865090acc6edceb4e7
GarlicBuns19 <planterx26@gmail.com>
2022-07-04 13:50:27 +0200

    help

--- Excercise 1/code.js
+++ Excercise 1/code.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     // Use template instead of typing in html
     template:`
-        <div v-if='isVisable' id="box1">
-            <h1>{{isVisable}}</h1>
-        </div>
             <h1>{{name}} {{surname}}</h1>
             <input type="text" v-model="name">
-        `
+            <button v-on:click= 'display'>Click Me</button>
+            <div v-if= 'isVisable' id="box1">
+                <h1>{{isVisable}}</h1>
+            </div>
+        `,
         // <button >Click Me</button>
-    ,data(){
+    data(){
         // Must use return
         return {
         name: 'Ra\'ees',
@@ -16,6 +17,12 @@ const app = Vue.createApp({
         name1: 'Jared',
         surname1: 'Isaacs',
         isVisable: true
+        }
+    },
+    methods: {
+        display(){
+            // Make use of this
+            this.isVisable = !this.isVisable;
         }
     }
 })
