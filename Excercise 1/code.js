ba59b2e8922732ef98ecc1b308b10de3d3c50591
GarlicBuns19 <planterx26@gmail.com>
2022-07-04 15:43:22 +0200

    it right

--- Excercise 1/code.js
+++ Excercise 1/code.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp({
     // Use template instead of typing in html
-    template:`
-            <h1>{{name}} {{surname}}</h1>
-            <input type="text" v-model="name">
-            <button v-on:click= 'display'>Click Me</button>
-            <div v-if= 'isVisable' id="box1">
-                <h1>{{isVisable}}</h1>
-            </div>
-        `,
+    // template:`
+    //         <h1>{{name}} {{surname}}</h1>
+    //         <input type="text" v-model="name">
+    //         <button v-on:click= 'display'>Click Me</button>
+    //         <div v-if= 'isVisable' id="box1">
+    //             <h1>{{isVisable}}</h1>
+    //         </div>
+    //     `,
         // <button >Click Me</button>
     data(){
         // Must use return
@@ -16,14 +16,41 @@ const app = Vue.createApp({
         surname: 'Benny',
         name1: 'Jared',
         surname1: 'Isaacs',
-        isVisable: true
+        isVisable: true,
+        thing: ' ',
+        age: '',
+        w: 'Lets Go',
+        q: 'You are not old enough'
         }
     },
-    methods: {
-        display(){
-            // Make use of this
-            this.isVisable = !this.isVisable;
-        }
-    }
+    // methods: {
+    //     display(){
+    //         // Make use of this
+    //         this.isVisable = !this.isVisable;
+    //     }
+    // },
+    template:`
+    Name<input type="text" v-model="thing">
+    Age<input type="number" v-model="age" id="age">
+        <h1>{{thing}}</h1>
+        <div v-if='age' id="box1">
+        <h2> {{age}}</h2>
+        </div>
+        <div id='output'>
+        </div>
+        <button v-on:click= 'ageDisplay'>Check</button>
+        `,
+        methods: {
+            ageDisplay(){
+                // Make use of this
+            if(age.value > 17){
+                document.getElementById('output').innerHTML = `<h2>${this.w}</h2>`
+                console.log(this.w)
+            }else{
+                document.getElementById('output').innerHTML = `<h2>${this.q}</h2>`
+                console.log(this.q)
+            }
+            }
+        },
 })
 app.mount('#app')
