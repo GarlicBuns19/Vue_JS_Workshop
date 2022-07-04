1f6b6ac23f700a5161b83cf1edc6282c583ab6e1
GarlicBuns19 <planterx26@gmail.com>
2022-07-04 13:41:18 +0200

    v-if=

--- Excercise 1/code.js
+++ Excercise 1/code.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
-    data(){
+    // Use template instead of typing in html
+    template:`
+        <div v-if='isVisable' id="box1">
+            <h1>{{isVisable}}</h1>
+        </div>
+            <h1>{{name}} {{surname}}</h1>
+            <input type="text" v-model="name">
+        `
+        // <button >Click Me</button>
+    ,data(){
         // Must use return
         return {
         name: 'Ra\'ees',
         surname: 'Benny',
         name1: 'Jared',
-        surname1: 'Isaacs'
+        surname1: 'Isaacs',
+        isVisable: true
         }
     }
 })
