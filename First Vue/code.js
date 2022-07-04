094bf2d208288a4cb0579d784f75da0bf3feb66e
GarlicBuns19 <planterx26@gmail.com>
2022-07-04 12:13:12 +0200

    Excercise

--- First Vue/code.js
+++ First Vue/code.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
+    
+    data(){
+        return {
+            greeting: 'Bonjour',
+            help: 'Plz Help',
+            time: 'Now, Like Right Now'
+        }
+    },
     data: function(){
         return {
-            greeting: 'Bonjour'
+            greeting: 'Bonjour',
+            help: 'Plz Help',
+            time: 'Now, Like Right Now'
         }
     }
 });
 // Mount it
-app.mount('#app')
+app.mount('#app');
+
+// Excercise
+// Create excercise that displays your name and surname
