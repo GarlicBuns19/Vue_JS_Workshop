0f90e56abc3708a88d9eb2426b2d99907497a51d
GarlicBuns19 <planterx26@gmail.com>
2022-07-06 19:01:23 +0100

    V-on

--- Eg/app.js
+++ Eg/app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
-    // data, functions
-    template: `<h2>I am a template</h2>`
-})
+  // data, functions
+//   template: `<h2>I am a template</h2>`,
+  data(){
+    return {
+        title: 'Final House',
+        author: 'Sam Jack',
+        age: 45
+    }
+  }
+});
 
-app.mount('#app')
+app.mount("#app");
