89c5970d5e2278598f0b05bc8aac9e0f0ef4af3a
GarlicBuns19 <planterx26@gmail.com>
2022-07-06 09:44:16 +0200

    It links up

--- VueCreateTest/property-listing/src/router/index.js
+++ VueCreateTest/property-listing/src/router/index.js
@@ -14,6 +14,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/single-property',
+    name: 'singleProperty',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SingleProperty.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue')
   }
 ]
 
---
+++ VueCreateTest/property-listing/src/views/SingleProperty.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="single-Property">
+    <h1>This is an Single Property page</h1>
+  </div>
+</template>
