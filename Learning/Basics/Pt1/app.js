01629615aeaa7ceb4a15ba93f20b38d1a5a751ce
GarlicBuns19 <planterx26@gmail.com>
2022-07-09 14:36:54 +0100

    Show if else in vue

--- Learning/Basics/Pt1/app.js
+++ Learning/Basics/Pt1/app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     // template: `<h1>Hello Vue ;)</h1>`
     data(){
         return {
+            show: true,
             title: 'Beans',
             age: 45
         }
@@ -9,6 +10,10 @@ const app = Vue.createApp({
     methods:{
         changeTitle(){
             this.title = 'Eat bread'
+        },
+        showItems(){
+            console.log('helo')
+            this.show = !this.show
         }
     }
 })
