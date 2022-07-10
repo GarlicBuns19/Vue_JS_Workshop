541bffe27750e499bcfdae93876c5078e86fe17f
GarlicBuns19 <planterx26@gmail.com>
2022-07-10 09:43:54 +0100

    claasList and setAttribute, ref call it $refs

--- Learning/Basics/Pt2 vue create/modal-project/src/App.vue
+++ Learning/Basics/Pt2 vue create/modal-project/src/App.vue
@@ -1,11 +1,13 @@
 <template>
 <h1>First Vue</h1>
 <h1>{{ subtitle }}</h1>
+<input type="text" name="" ref="name">
+<button @click="handelClick">Click Me</button>
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
-<script>
+<script>  
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -14,6 +16,14 @@ export default {
       return {
         subtitle: "Lets go",
       };
+   },
+   methods:{
+    handelClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.focus( )
+      this.$refs.name.setAttribute('id','input-ref')
+      this.$refs.name.classList.add('input-refffff')
+    }
    }
   // components: {
   //   HelloWorld
